56758ef4c7d09193d36873bfe38e854de8e4b562
RamiroArambarri <raarambarri@gmail.com>
2025-01-02 16:00:25 -0300

    corrigiendo imagen no encontrada 1

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -1,123 +1,123 @@
 export const galleryData = [
     [
         {
-            src: '../images/digital/ultima_cancion.png',
+            src: 'illustration-portfolio/images/digital/ultima_cancion.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/zorro.png',
+            src: 'illustration-portfolio/images/digital/zorro.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/trigo.png',
+            src: 'illustration-portfolio/images/digital/trigo.png',
             thumnailSize: '150%'
         },
         {
-            src: '../images/digital/telefono.png',
+            src: '/images/digital/telefono.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/tren.png',
+            src: '/images/digital/tren.png',
             thumnailSize: '150%'
         },
         {
-            src: '../images/digital/neko.png',
+            src: '/images/digital/neko.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/lobo.png',
+            src: '/images/digital/lobo.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/sirena.png',
+            src: '/images/digital/sirena.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/quetzal.png',
+            src: '/images/digital/quetzal.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/guardian.png',
+            src: '/images/digital/guardian.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/cthulhu.png',
+            src: '/images/digital/cthulhu.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/digital/paladin.png',
+            src: '/images/digital/paladin.png',
             thumnailSize: '100%'
         },
     ],
     [
         {
-            src: '../images/pixel-art/halloween.png',
+            src: '/images/pixel-art/halloween.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/pixel-art/pandora.png',
+            src: '/images/pixel-art/pandora.png',
             thumnailSize: '175%'
         },
         {
-            src: '../images/pixel-art/bitsonia.png',
+            src: '/images/pixel-art/bitsonia.png',
             thumnailSize: '175%'
         },
         {
-            src: '../images/pixel-art/dragon.jpg',
+            src: '/images/pixel-art/dragon.jpg',
             thumnailSize: '100%'
         },
         {
-            src: '../images/pixel-art/himeji.png',
+            src: '/images/pixel-art/himeji.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/pixel-art/salon.png',
+            src: '/images/pixel-art/salon.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/pixel-art/tetera.png',
+            src: '/images/pixel-art/tetera.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/pixel-art/noctambularia.jpg',
+            src: '/images/pixel-art/noctambularia.jpg',
             thumnailSize: '175%'
         },
     ],
     [
         {
-            src: '../images/sketch/lechuza-sketch.png',
+            src: '/images/sketch/lechuza-sketch.png',
             thumnailSize: '120%'
         },
         {
-            src: '../images/sketch/mouth-sketch.jpg',
+            src: '/images/sketch/mouth-sketch.jpg',
             thumnailSize: '120%'
         },
         {
-            src: '../images/sketch/dragon-sketch.jpg',
+            src: '/images/sketch/dragon-sketch.jpg',
             thumnailSize: '100%'
         },
         {
-            src: '../images/sketch/buho-sketch.png',
+            src: '/images/sketch/buho-sketch.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/sketch/angel-sketch.jpg',
+            src: '/images/sketch/angel-sketch.jpg',
             thumnailSize: '100%'
         },
         {
-            src: '../images/sketch/guardian-sketch.png',
+            src: '/images/sketch/guardian-sketch.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/sketch/landscape-1.jpg',
+            src: '/images/sketch/landscape-1.jpg',
             thumnailSize: '100%'
         },
         {
-            src: '../images/sketch/parque-sketch.png',
+            src: '/images/sketch/parque-sketch.png',
             thumnailSize: '100%'
         },
         {
-            src: '../images/sketch/landscape-2.jpg',
+            src: '/images/sketch/landscape-2.jpg',
             thumnailSize: '100%'
         },
     ]
