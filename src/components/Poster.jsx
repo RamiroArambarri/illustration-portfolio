541410b65b0c10a63b5d46ac6c6473f4c6a8f14e
RamiroArambarri <raarambarri@gmail.com>
2025-01-06 19:47:14 -0300

    corrijo estilos vista poster

--- src/components/Poster.jsx
+++ src/components/Poster.jsx
@@ -33,12 +33,14 @@ const Poster = ({ index, setShowPoster, gallery }) => {
   } else {
     return (
       <>
-        <div className={posterStyles['poster-container']} onClick={() => { setShowPoster(-1) }}>
-          <img className={`${posterStyles.close} ${posterStyles['poster-button-hover']}`} src={xImage} onClick={() => { setShowPoster(-1) }} role="button" />
-          <img className={posterStyles['poster']} src={galleryData[gallery][index].src} onClick={ev => ev.stopPropagation()} />
-          <div className={posterStyles['bottom-block']}>
-            <img className={posterStyles['poster-button-hover']} src={prevImage} onClick={(ev) => { changePoster(-1, ev) }} role="button" />
-            <img src={nextImage} alt="next" className={posterStyles['poster-button-hover']} onClick={(ev) => { changePoster(1, ev) }} role="button" />
+        <div className={posterStyles['poster-background']} onClick={() => { setShowPoster(-1) }}>
+          <div className={posterStyles['poster-container']} onClick={() => { setShowPoster(-1) }}>
+            <img className={`${posterStyles.close} ${posterStyles['poster-button-hover']}`} src={xImage} onClick={() => { setShowPoster(-1) }} role="button" />
+            <img className={posterStyles['poster']} src={galleryData[gallery][index].src} onClick={ev => ev.stopPropagation()} />
+            <div className={posterStyles['bottom-block']}>
+              <img className={posterStyles['poster-button-hover']} src={prevImage} onClick={(ev) => { changePoster(-1, ev) }} role="button" />
+              <img src={nextImage} alt="next" className={posterStyles['poster-button-hover']} onClick={(ev) => { changePoster(1, ev) }} role="button" />
+            </div>
           </div>
         </div>
       </>
